a6a82d9cfbe41ebc54500e2f4604835525a88861
Viibur <viiburandre@gmail.com>
2023-03-22 19:25:42 +0200

    BE muudatused

---
+++ src/main/java/com/example/projectbe/model/Veanaide.java
@@ -0,0 +1,23 @@
+package com.example.projectbe.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(schema = "public")
+public class Veanaide {
+    @Id
+    @Column(insertable = false, updatable = false, unique = true)
+    @SequenceGenerator(name = "veanaide_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "veanaide_seq")
+    private Long id;
+    private String korrektne;
+    private String viga;
+    private String lause;
+    private int sagedus;
+}
---
+++ src/main/java/com/example/projectbe/model/VeanaideDTO.java
@@ -0,0 +1,14 @@
+package com.example.projectbe.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class VeanaideDTO {
+    private Long id;
+    private String korrektne;
+    private String viga;
+    private String lause;
+    private int sagedus;
+}
